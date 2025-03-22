abf0b4703f0739d368c622c0d4911976e966ecf2
Saurabh <sn2919@att.com>
2025-03-23 05:12:24 +0530

    Initial commit for ProductService

---
+++ src/main/java/com/productservice/productservice/controller/ProductController.java
@@ -0,0 +1,40 @@
+package com.productservice.productservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+
+    @GetMapping
+    public String home() {
+        return "Welcome to Product Service!";
+    }
+
+
+    public void getProductById(){
+
+    }
+
+    public void getAllProducts(){
+
+    }
+
+    public void deleteProductById(){
+
+    }
+
+    public void createProduct(){
+
+    }
+
+    public void updateProductById(){
+
+    }
+
+
+}
