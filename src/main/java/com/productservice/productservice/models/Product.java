6e7a71fb9789093c947e7bd22df64ea77757b1da
Saurabh Nikhade <74154464+saurabh22-star@users.noreply.github.com>
2025-03-23 05:12:24 +0530

    Initial commit for ProductService

---
+++ src/main/java/com/productservice/productservice/models/Product.java
@@ -0,0 +1,20 @@
+package com.productservice.productservice.models;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product extends BaseModel{
+
+    private String title;
+
+    private String description;
+
+    private int price;
+
+    private  String image;
+
+    private Category category;
+}
