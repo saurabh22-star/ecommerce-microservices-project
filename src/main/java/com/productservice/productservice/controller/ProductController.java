2696a492d027694b177fda1082a4b931e4e0dbba
Saurabh Nikhade <74154464+saurabh22-star@users.noreply.github.com>
2025-03-24 03:50:46 +0530

    Added Product Service Interface and Impl class

--- src/main/java/com/productservice/productservice/controller/ProductController.java
+++ src/main/java/com/productservice/productservice/controller/ProductController.java
@@ -1,32 +1,45 @@
 package com.productservice.productservice.controller;
 
+import com.productservice.productservice.services.FakeStoreProductService;
+import com.productservice.productservice.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-    //Controller for the project
-    @GetMapping
+
+    private ProductService productService;
+
+    //Constructor Injection
+    ProductController(@Qualifier("fakeStoreProductService") ProductService productService){
+        this.productService = productService;
+    }
+
+    @GetMapping("/home")
     public String home() {
         return "Welcome to Product Service!";
     }
 
-
-    public void getProductById(){
-
+    @GetMapping("/{id}")
+    public String getProductById(@PathVariable("id") Long id){
+        return productService.getProductById(id);
     }
 
+
+    @GetMapping
     public void getAllProducts(){
 
     }
 
+    @DeleteMapping("/{id}")
     public void deleteProductById(){
 
     }
+
 
     public void createProduct(){
 
